c1472c81f7a84b446b548af02551497fddbb57db
Anton Kazakevich <ant.kazakevich@yandex.ru>
2025-06-24 16:16:28 +0700

    Update onboarding.ts

--- src/handlers/onboarding.ts
+++ src/handlers/onboarding.ts
@@ -1,7 +1,10 @@
 import { Context } from "telegraf";
 import { upsertUser } from "../db/queries/users";
 import { posthog } from "../posthog";
+import fs from "fs";
 import path from "path";
+
+const imageBuffer = fs.readFileSync(path.resolve(__dirname, "../assets/welcome.png"));
 
 export const onboardingHandler = async (ctx: Context) => {
     await upsertUser(ctx);
@@ -16,15 +19,13 @@ export const onboardingHandler = async (ctx: Context) => {
     });
 
     await ctx.replyWithPhoto(
-        {
-            source: path.resolve(__dirname, "../welcome.png") // ← переименуй файл без пробелов
-        },
+        { source: imageBuffer },
         {
             caption:
                 `👋 Привет! Я — *Inflexo Bot*.\n\n` +
                 `Готов помочь вам узнать всё о вашей аудитории.\n\n` +
                 `🔍 Введите *@username* канала или группы, которую хотите проанализировать.`,
-            parse_mode: "MarkdownV2"
+            parse_mode: "Markdown",
         }
     );
 };
